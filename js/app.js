132bc67c8e5d4efd3c1567071e34acd948327cd9
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-15 23:14:35 +0200

    CSS variables can be modified by js, hurray

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 
+    const allInputs = document.querySelectorAll('.controls input');
+    
+    handleValue = (e) => {
+        const suffix = e.target.dataset.size || ''; 
+
+        document.documentElement.style.setProperty(`--${e.target.name}`, e.target.value + suffix);
+    }
+    
+//    Listen for changes on input element
+    
+    allInputs.forEach(input => input.addEventListener("change", handleValue));    
+    
+//    Listens for changes when mouse is used
+    
+    allInputs.forEach(input => input.addEventListener("mousemove", handleValue));
 });
